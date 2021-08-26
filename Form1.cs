170e430d4e7daf646c207fd178032319d42c29bf
Valentina2103 <80237175+Valentina2103@users.noreply.github.com>
2021-08-26 12:53:02 -0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParadigmaEventos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Santiago Jagua Carvajal");
+            comboBox1.Items.Add("Jose Daniel Cabrera Calderon");
+            comboBox1.Items.Add("Valentina Peñuela Navas");
+            comboBox1.Items.Add("Daniel Felipe Millan Martinez");
+            comboBox1.Items.Add("Felix Andres Asela Garcia");
+            comboBox1.Items.Add("Daniela Lucia Arenas Gomez");
+            comboBox1.Items.Add("Karen Nicol Dayanna Lizarazo Ortiz");
+            comboBox1.Items.Add("Carlos Stevenz Rua Garcia");
+            comboBox1.Items.Add("Diego Felipe Lara Angarita);
+            comboBox1.Items.Add("Santiago Jagua Carvajal");
+            comboBox1.Items.Add("Juan Sebastian Perez Millares");
+            comboBox1.Items.Add("Juan David Macias Calderon");
+            comboBox1.Items.Add("Harold Enrique Niño Aristizabal");
+            comboBox1.Items.Add("Juan Sebastian Pasichana Moncada");
+            comboBox1.Items.Add("Andres Essneider Quintero Valderrama");
+            comboBox1.Items.Add("Juan Pablo Adames Solarte");
+            comboBox1.Items.Add("Diego Alejandro Pinzon Gonzalez");
+            comboBox1.Items.Add("Jesus Enrique Bonilla España");
+            comboBox1.Items.Add("Yarith Fernanda Espitia Ortiz");
+            comboBox1.Items.Add("Nicolas Elias Bossio Rincon");
+            comboBox1.Items.Add("Andres Santiago Lopez Lopez");
+            comboBox1.Items.Add("Edinson Felipe Lazaro Gonzales");
+            comboBox1.Items.Add("Juan David Acuña Merchan");
+            comboBox1.Items.Add("Juan Sebastian Tarazona Quintero");
+            comboBox1.Items.Add("Jorge Andres Pinilla Gonzalez");
+            comboBox1.Items.Add("Edwin Nicolas Tarazona Morales");
+            comboBox1.Items.Add("David Alejandro Pinzon Gonzalez");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
