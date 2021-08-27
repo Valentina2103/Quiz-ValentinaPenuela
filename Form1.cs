4c8a1c368ceecc4c333bbb70cc08e5c3450d1c90
Valentina2103 <80237175+Valentina2103@users.noreply.github.com>
2021-08-27 15:10:54 -0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParadigmaEventos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbName.Items.Add("Santiago Jagua Carvajal");
+            cbName.Items.Add("Jose Daniel Cabrera Calderon");
+            cbName.Items.Add("Valentina Peñuela Navas");
+            cbName.Items.Add("Daniel Felipe Millan Martinez");
+            cbName.Items.Add("Felix Andres Asela Garcia");
+            cbName.Items.Add("Daniela Lucia Arenas Gomez");
+            cbName.Items.Add("Karen Nicol Dayanna Lizarazo Ortiz");
+            cbName.Items.Add("Carlos Stevenz Rua Garcia");
+            cbName.Items.Add("Diego Felipe Lara Angarita");
+            cbName.Items.Add("Juan Sebastian Perez Millares");
+            cbName.Items.Add("Juan David Macias Calderon");
+            cbName.Items.Add("Harold Enrique Niño Aristizabal");
+            cbName.Items.Add("Juan Sebastian Pasichana Moncada");
+            cbName.Items.Add("Andres Essneider Quintero Valderrama");
+            cbName.Items.Add("Juan Pablo Adames Solarte");
+            cbName.Items.Add("Diego Alejandro Pinzon Gonzalez");
+            cbName.Items.Add("Jesus Enrique Bonilla España");
+            cbName.Items.Add("Yarith Fernanda Espitia Ortiz");
+            cbName.Items.Add("Nicolas Elias Bossio Rincon");
+            cbName.Items.Add("Andres Santiago Lopez Lopez");
+            cbName.Items.Add("Edinson Felipe Lazaro Gonzales");
+            cbName.Items.Add("Juan Felipe Meza Morales");
+            cbName.Items.Add("Juan David Acuña Merchan");
+            cbName.Items.Add("Juan Sebastian Tarazona Quintero");
+            cbName.Items.Add("Jorge Andres Pinilla Gonzalez");
+            cbName.Items.Add("Edwin Nicolas Tarazona Morales");
+            cbName.Items.Add("David Alejandro Pinzon Gonzalez");
+            cbName.Text = "Selecciona tu nombre";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string frase = "Nombre: "
+            + Convert.ToString(cbName.SelectedItem) + " - " + " Documento: "
+            + Convert.ToString(tbDocumento.Text) + " - " + " Fecha de nacimiento: "
+            + Convert.ToString(dtpFechaNacimiento.Value) + " - " + "NRC: "
+            + Convert.ToString(tbNRC.Text);
+
+            rtxtResults.Text = frase + rtxtResults.Text;
+
+            cbName.Text = "Selecciona tu nombre";
+            tbDocumento.Clear();
+            dtpFechaNacimiento.Value = DateTime.Today;
+            tbNRC.Clear();
+        }
+
+        private void btnReport_Click(object sender, EventArgs e)
+        {
+            Form2 Formulario = new Form2();
+            Formulario.Show();
+
+            Form2 f = new Form2();
+            f.tbDatos.Text = rtxtResults.Text;
+            this.Hide();
+            f.ShowDialog();
+
+        }
+
+    }
+}
+
+
+
+
+
+
---
+++ Form2.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace ParadigmaEventos
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbDatos = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mongolian Baiti", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(142, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(515, 43);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Datos finales del RichTextBox";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tbDatos
+            // 
+            this.tbDatos.Font = new System.Drawing.Font("Mongolian Baiti", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbDatos.Location = new System.Drawing.Point(56, 97);
+            this.tbDatos.Multiline = true;
+            this.tbDatos.Name = "tbDatos";
+            this.tbDatos.Size = new System.Drawing.Size(700, 465);
+            this.tbDatos.TabIndex = 1;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 624);
+            this.Controls.Add(this.tbDatos);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Report";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.TextBox tbDatos;
+    }
+}
